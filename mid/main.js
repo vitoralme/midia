2251741521f6b4abcb3ed88c1fb70607ae1e49eb
Vitor Almeida da Silva <51917475+vitoralme@users.noreply.github.com>
2023-06-19 11:24:48 -0300

    Add files via upload

--- mid/main.js
+++ mid/main.js
@@ -10,7 +10,17 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
-    listaDeTeclas[contador].onclick = function () {
+   tecla.onclick = function () {
         tocaSom(idAudio)
+    }
+
+    tecla.onkeydown = function (event){
+        if (event.code === 'Space'|| event.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 }
